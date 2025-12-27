79c5e0e6e3489f23a7ea0be420a3f1961de8f83d
suki2811 <sukeerthiadireddi@gmail.com>
2025-12-27 17:11:35 +0530

    Initial aesthetic Resume Analyzer UI

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <div
+      style={{
+        fontSize: "20px",
+        fontWeight: "bold",
+        paddingBottom: "20px",
+        borderBottom: "1px solid #1f2937",
+      }}
+    >
+      Resume Analyzer
+    </div>
+  );
+}
