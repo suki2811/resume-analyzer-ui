918db9e806674b5b11f023cadbc2e57fd7442fca
suki2811 <sukeerthiadireddi@gmail.com>
2025-12-27 17:12:35 +0530

    Initial aesthetic Resume Analyzer UI

---
+++ src/components/UploadCard.jsx
@@ -0,0 +1,14 @@
+export default function UploadCard() {
+  return (
+    <div
+      style={{
+        background: "#161a22",
+        padding: "20px",
+        borderRadius: "12px",
+      }}
+    >
+      <h3>Upload Resume</h3>
+      <input type="file" />
+    </div>
+  );
+}
