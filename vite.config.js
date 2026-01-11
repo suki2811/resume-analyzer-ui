7259c775787d2f350c313e39bf06b9eaee198b10
Sukeerthi Adireddi <sukeerthiadireddi@gmail.com>
2026-01-11 17:50:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/resume-analyzer-ui/"
+  base: "/resume-analyzer-ui/",
+  build: {
+    assetsDir: "assets"
+  }
 });
 
